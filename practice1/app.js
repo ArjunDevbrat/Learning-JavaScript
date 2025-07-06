b8d7f627b6ca6fff31e6bb71b6e3b599918b721b
Arjun Devbrat <160641644+ArjunDevbrat@users.noreply.github.com>
2025-07-07 00:41:37 +0530

    Add files via upload
    Practicing basic JS syntax and getting started with console-based output.

---
+++ practice1/app.js
@@ -0,0 +1,12 @@
+let color ="Green";
+
+//traffic light 
+if  (color =="red"){
+    console.log("stop.color is red");
+}
+if (color =="yellow"){
+    console.log("slow down .color is yellow");
+}
+if (color == "Green"){
+    console.log("Go.color is green");
+}
