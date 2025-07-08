96d9f56d7a0515d862142fdb2448acdce49c7d19
Arjun Devbrat <160641644+ArjunDevbrat@users.noreply.github.com>
2025-07-09 00:04:06 +0530

    Add files via upload
    switch  case

---
+++ practice3/app.js
@@ -0,0 +1,16 @@
+let color ="red";
+
+switch(color){
+  case "red":
+    console.log("stop");
+    break;
+  case "yellow":
+    console.log("slowdown");
+    break;
+  case "green":
+    console.log("go");
+    break;   
+  default:
+    console.log("light is broken");
+    break;
+}
