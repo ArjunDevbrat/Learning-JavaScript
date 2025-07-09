d4d74fd6b9d1fce7e0a175bd331e8e1e5655debe
Arjun Devbrat <160641644+ArjunDevbrat@users.noreply.github.com>
2025-07-10 01:21:42 +0530

    basic string
    basic string use in js

---
+++ practiceString/app.js
@@ -0,0 +1,11 @@
+// trim & convert it to uppercase.
+let msg = "  hello  ";
+console.log(msg.trim().toUpperCase());
+
+
+//name.slice(),name  .index,name.replace
+ let name =" ArjunDevbrat";
+ //work on console
+ // name.slice(4,6);
+ //name.indexof("un");
+ //name.replace('j','k');
